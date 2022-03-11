18274be4c71acba36a4226bbd04cc12a5712f8a2
MichalMurin <murin.michal.945@gmail.com>
2022-03-11 11:15:11 +0100

    Add project files.

---
+++ LLMSapp/LLMSapp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using LLMSapp.ViewModels;
+using LLMSapp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace LLMSapp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
